c33742fe035fa9d4b724348541a5d019c284f4f1
abdulkerim <abdu0921@hotmail.com>
2023-12-20 15:54:22 -0500

    first commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	public static Logger logger = LoggerFactory.getLogger(DemoApplication.class);
+	@GetMapping("/message")
+	public String sayHi(){
+		logger.info("Application executed");
+		return "<h1 style='color:red; font-size:50px; text-align:center'>Hello, World!</h1>";
+	}
+}
